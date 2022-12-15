36c41d27abb2a468588fbe0a8fdd95d6bd2d14b7
James Bogema <james.bogema.lvyx@statefarm.com>
2022-12-15 11:55:06 -0600

    modified terraform block of the main.tf file per instructions

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
+        }
     }
-  }
+    backend "http" {
+    }
 }
 
 provider "aws" {
